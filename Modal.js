d186e7388ffeec8e51b7dbc2efeeb9e27d303182
sspantz <sspantiezheng@gmail.com>
2021-05-28 10:44:50 +0800

    Create Modal

---
+++ Modal.js
@@ -0,0 +1,23 @@
+function Modal(
+  config = {
+    title: "Modal Title",
+    message: "Real programmers count from 0.",
+  }
+) {
+  const { title, message } = config;
+
+  let Div = document.createElement("dialog");
+
+  let Title = document.createElement("h3");
+  Title.textContent = title;
+
+  let Message = document.createElement("p");
+  Message.textContent = message;
+
+  Div.appendChild(Title);
+  Div.appendChild(Message);
+
+  return Div;
+}
+
+console.log("OK!");
